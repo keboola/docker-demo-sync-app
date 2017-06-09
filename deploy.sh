90fa6cdff4121515cfc90df16a48115c32332342
Martin Halamíček <martin@halamicek.cz>
2017-06-09 10:47:16 +0200

    push also to dev AWS

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,11 @@ docker tag keboola/docker-demo-sync-app:latest $REPOSITORY:latest
 eval $(docker run --rm -e KBC_DEVELOPERPORTAL_USERNAME=$KBC_DEVELOPERPORTAL_USERNAME -e KBC_DEVELOPERPORTAL_PASSWORD=$KBC_DEVELOPERPORTAL_PASSWORD -e KBC_DEVELOPERPORTAL_URL=$KBC_DEVELOPERPORTAL_URL quay.io/keboola/developer-portal-cli-v2:0.0.1 ecr:get-login keboola keboola.docker-demo-sync)
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
+
+# deploy to testing AWS account
+# needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+eval $(aws ecr get-login --region us-east-1)
+docker tag keboola/docker-demo-sync-app:latest 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/keboola.docker-demo-sync:$TRAVIS_TAG
+docker tag keboola/docker-demo-sync-app:latest 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/keboola.docker-demo-sync:latest
+docker push 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/keboola.docker-demo-sync:$TRAVIS_TAG
+docker push 061240556736.dkr.ecr.us-east-1.amazonaws.com/keboola/keboola.docker-demo-sync:latest
