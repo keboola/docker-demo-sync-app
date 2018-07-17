753b96472d6253cc5255a3989fe6d5828dc2c7a3
Odin <odinuv@gmail.com>
2018-07-17 14:21:23 +0200

    feat: merge code from https://github.com/keboola/docker-actions-test

--- src/run.php
+++ src/run.php
@@ -1,6 +1,6 @@
 <?php
 
-$arguments = getopt("d::", array("data::"));
+$arguments = getopt("d::", ["data::"]);
 if (!isset($arguments["data"])) {
     print "Data folder not set.";
     exit(1);
@@ -13,6 +13,42 @@ if ($config['action'] === 'dump') {
         'output' => 'ok',
     ]);
     exit(0);
+} elseif ($config['action'] === 'test') {
+    print json_encode([
+        'test' => 'test',
+    ]);
+    exit(0);
+} elseif ($config['action'] === 'timeout') {
+    sleep(60);
+    exit(0);
+} elseif ($config['action'] === 'emptyjsonarray') {
+    print '[]';
+    exit(0);
+} elseif ($config['action'] === 'emptyjsonobject') {
+    print '{}';
+    exit(0);
+} elseif ($config['action'] === 'invalidjson') {
+    print '{"tables": ["a", "b", "c"]';
+    exit(0);
+} elseif ($config['action'] === 'noresponse') {
+    exit(0);
+} elseif ($config['action'] === 'usererror') {
+    print('user error');
+    exit(1);
+} elseif ($config['action'] === 'apperror') {
+    print('application error');
+    exit(2);
+} elseif ($config['action'] === 'decrypt') {
+    if ($config['parameters']['#password'] === 'password') {
+        print json_encode([
+            'password' => $config['parameters']['#password'],
+        ]);
+        exit(0);
+    } else {
+        print('failed');
+        exit(1);
+    }
+} else {
+    print sprintf("Action %s is not supported", $config['action']);
+    exit(1);
 }
-print sprintf("Action %s is not supported", $config['action']);
-exit(1);
